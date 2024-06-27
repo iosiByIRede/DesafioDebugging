b872c3d97865e930d23caf8ce3d9a53e51072ae5
gabrielsantiago4 <gaaasts@gmail.com>
2024-06-27 11:49:38 -0300

    Initial Commit

---
+++ DesafioDebugging/DesafioDebuggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesafioDebuggingApp.swift
+//  DesafioDebugging
+//
+//  Created by Gabriel Santiago on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct DesafioDebuggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
